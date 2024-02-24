f7210d25216150b5161760cdba4de9b876ca5f7c
Eber Brown <eaba_89@hotmail.com>
2024-02-24 11:34:37 +0100

    js code for mobile burger menu

---
+++ components/navbar.js
@@ -0,0 +1,37 @@
+const navBarBurgerEl = document.querySelector(".nav-burger-menu");
+
+function navBarBurger() {
+  var windowWidth = window.innerWidth;
+  const navBarLinksEl = document.querySelector(".nav-bar-links");
+
+  if (windowWidth < 991) {
+    navBarLinksEl.classList.add("no-show");
+    navBarBurgerEl.classList.remove("no-show");
+  } else {
+    navBarLinksEl.classList.remove("no-show");
+    navBarBurgerEl.classList.add("no-show");
+  }
+}
+
+let shownBurgerMenu = false;
+function showBurgerMenu() {
+  const navBurgerContainerEl = document.querySelector(".burger-menu-container");
+
+  if (shownBurgerMenu === false) {
+    shownBurgerMenu = true;
+    navBurgerContainerEl.classList.remove("no-show");
+  } else if (shownBurgerMenu === true) {
+    shownBurgerMenu = false;
+    navBurgerContainerEl.classList.add("no-show");
+  }
+}
+
+navBarBurger();
+
+window.addEventListener("resize", () => {
+  navBarBurger();
+});
+
+navBarBurgerEl.addEventListener("click", () => {
+    showBurgerMenu();
+  });
